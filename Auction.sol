894a463c0045af1b7fc3ffab7fc85f9d31b667ab
krrdash <45856150+krrdash@users.noreply.github.com>
2021-07-05 23:45:49 +0200

    Update Auction.sol
    added deposit() for users to pay vadium
    changed withdrawing method

--- Auction.sol
+++ Auction.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
+
 contract Auction {
     address payable public beneficiary;
     uint public auctionEndTime;
+    uint public bidBondInEthers;
+    string public hashOfAuction;
 
     address public highestBidder;
+
     uint public highestBid;
 
-    mapping(address => uint) pendingReturns;
+    mapping(address => uint) depositCashback;
+    mapping(address => bool) isEligible;
 
     bool ended;
 
@@ -18,39 +23,71 @@ contract Auction {
     error BidNotHighEnough(uint highestBid);
     error AuctionNotYetEnded();
     error AuctionEndAlreadyCalled();
+    error IncorrectBidBondValue(uint bidBondInEthers);
+    error UserIsNotEligible(address user);
+    error SellerCantBeBuyer(address beneficiary);
 
   
-    constructor(uint _biddingTime, address payable _beneficiary) {
+    constructor(address payable _beneficiary, uint _biddingTime, uint _bidBond, string memory _hashOfAuction) {
         beneficiary = _beneficiary;
         auctionEndTime = block.timestamp + (_biddingTime * 60 seconds);
+        bidBondInEthers = _bidBond * 1 ether;
+        hashOfAuction = _hashOfAuction;
     }
 
+    function deposit() public payable {
+        
+        if (block.timestamp > auctionEndTime)
+            revert AuctionAlreadyEnded();
+            
+        if(msg.value * 1 ether != bidBondInEthers)
+            revert IncorrectBidBondValue(bidBondInEthers);
+            
+        if(msg.sender == beneficiary)
+            revert SellerCantBeBuyer(beneficiary);
+            
+        
+        isEligible[msg.sender] = true;
+        depositCashback[msg.sender] += msg.value;
+        
+    }
+    
     function bid() public payable {
      
         if (block.timestamp > auctionEndTime)
             revert AuctionAlreadyEnded();
 
-      
         if (msg.value <= highestBid)
             revert BidNotHighEnough(highestBid);
+        
+        if(isEligible[msg.sender] == false)
+            revert UserIsNotEligible(msg.sender);
 
-        if (highestBid != 0) {
-            pendingReturns[highestBidder] += highestBid;
-        
-        
+        if (highestBid != 0) 
+            payable(highestBidder).transfer(highestBid);
+     
         highestBidder = msg.sender;
         highestBid = msg.value;
         emit HighestBidIncreased(msg.sender, msg.value);
+    
     }
 
     
-    function withdraw() public returns (bool) {
-        uint amount = pendingReturns[msg.sender];
+    function withdrawBidBond() public returns (bool) {
+        
+        if (ended == false)
+            revert AuctionNotYetEnded();
+            
+        if(msg.sender == highestBidder)
+            depositCashback[msg.sender] = 0;
+        
+        uint amount = depositCashback[msg.sender];
+        
         if (amount > 0) {
-            pendingReturns[msg.sender] = 0;
+            depositCashback[msg.sender] = 0;
 
             if (!payable(msg.sender).send(amount)) {
-                pendingReturns[msg.sender] = amount;
+                depositCashback[msg.sender] = amount;
                 return false;
             }
         }
@@ -58,6 +95,7 @@ contract Auction {
     }
 
     function auctionEnd() public {
+        
         if (block.timestamp < auctionEndTime)
             revert AuctionNotYetEnded();
         if (ended)
